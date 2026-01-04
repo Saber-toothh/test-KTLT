7ca77238c301e411a415d59738500c8e3637efd0
Pipi <ducanhyh5a@gmail.com>
2026-01-04 21:53:40 +0700

    Update LoadConfig.cpp

--- src/LoadConfig.cpp
+++ src/LoadConfig.cpp
@@ -7,16 +7,17 @@ Config loadConfig(const std::string& filePath) {
     std::ifstream file(filePath);
     
     if (!file.is_open()) {
-        std::cerr << "[Loi Module 1] Khong tim thay file config: " << filePath << std::endl;
+        std::cerr << "[Loi] Khong tim thay file config: " << filePath << std::endl;
         return Config(); 
     }
 
     json j;
     file >> j; // Đọc toàn bộ file vào biến json
 
-    Config cfg = j.get<Config>(); 
+    Config cfg = j.get<Config>(); // nhập tự động dùng MACRO JSON
 
-    std::cout << "[Module 1] Da load config: " << cfg.simulation_name << std::endl;
+    std::cout << "[OK] Da load config: " << cfg.simulation_name << std::endl;
     
     return cfg;
 }
+
