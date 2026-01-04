bb9cc9a6c7bf018b3d17bb947977c83353f7d9ee
Pipi <ducanhyh5a@gmail.com>
2026-01-04 21:31:46 +0700

    Update Config.h

--- include/Config.h
+++ include/Config.h
@@ -1,22 +1,20 @@
-// --- File: Config.h ---
 #ifndef CONFIG_H
 #define CONFIG_H
 
 #include <string>
 #include <iostream>
 #include <fstream>
-#include "json.hpp" // Đảm bảo đường dẫn đúng
+#include "json.hpp"
 
 using json = nlohmann::json;
 
-// 1. Tạo struct con cho phần traffic_rate (vì nó lồng bên trong)
 struct TrafficRate {
     double data;
     double video;
     double voice;
 };
 
-// 2. Tạo struct Config chính
+// Cấu trúc Config chính
 struct Config {
     std::string simulation_name;
     double total_time;
@@ -25,12 +23,11 @@ struct Config {
     TrafficRate traffic_rate; // Lồng struct con vào đây
 };
 
-// 3. KHAI BÁO MACRO (Phép thuật nằm ở đây)
-// Lưu ý: Phải khai báo TrafficRate trước thì Config mới hiểu
+// KHAI BÁO MACRO
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(TrafficRate, data, video, voice)
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Config, simulation_name, total_time, bandwidth_mbps, max_queue_size, traffic_rate)
 
-// 4. Khai báo hàm load (chỉ tên hàm)
+// Khai báo hàm loadConfig
 Config loadConfig(const std::string& filePath);
 
 #endif
