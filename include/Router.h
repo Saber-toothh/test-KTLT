5bd5f719cd7fc63f8155ea61a115a5b81f7862f7
Pipi <ducanhyh5a@gmail.com>
2026-01-04 21:44:49 +0700

    Update Router.h

--- include/Router.h
+++ include/Router.h
@@ -8,28 +8,28 @@
 
 class Router {
 private:
-    // Hàng đợi ưu tiên (Tự động sắp xếp VOICE lên đầu, DATA xuống cuối nhờ operator< trong Packet)
+    // Hàng đợi ưu tiên: Tự động sắp xếp VOICE lên đầu, DATA xuống cuối qua operator<
     std::priority_queue<Packet> waitingQueue;
     
-    // Lưu lịch sử các gói đã xử lý xong để Module 4 báo cáo
+    // Lưu lịch sử các gói đã xử lý xong để Phân tích
     std::vector<Packet> processedHistory;
 
     Config cfg; 
     
-    // Biến trạng thái: Router đang bận đến thời điểm nào?
+    // Biến trạng thái: Router đang bận xử lý gói hiện tại đến ?
     double busyUntil; 
 
 public:
     // Constructor nhận Config
     Router(const Config& config);
 
-    // 1. Nhận gói tin từ Generator (Ingress)
+    // Nhận gói tin từ PacketGenerator
     void receivePacket(Packet p);
 
-    // 2. Xử lý gói tin theo thời gian thực (Core Logic)
+    // Xử lý gói tin theo thời gian thực
     void process(double currentTime);
 
-    // 3. Lấy dữ liệu lịch sử
+    // Lấy dữ liệu lịch sử
     std::vector<Packet> getHistory() const;
 };
 
