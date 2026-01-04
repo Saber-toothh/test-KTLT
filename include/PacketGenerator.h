2597eb66ec73f626ac48471cd0eca2ce9bbe99f2
Pipi <ducanhyh5a@gmail.com>
2026-01-04 21:42:31 +0700

    Update PacketGenerator.h

--- include/PacketGenerator.h
+++ include/PacketGenerator.h
@@ -4,17 +4,7 @@
 #include "Packet.h"
 #include "Config.h"
 
-// Hàm sinh một gói tin dựa trên cấu hình lưu lượng
+// Hàm sinh MỘT gói tin dựa trên cấu hình lưu lượng
 Packet generatePacket(int id, double currentTime, const Config& cfg);
 
-#endif
-
-/* 
-Input :  - Id 
-		 - currentTim: thời điểm tới router
-		 - TrafficConfig: cấu hình lưu lượng 
-		
-Output : - 1 gói tin hoàn chỉnh đấy đủ dữ liệu
-*/			 
-				              		 
-						
+#endif			              		 				
