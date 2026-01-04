9bb54a51053aefcafa57b7c73f3be91be136ec13
Pipi <ducanhyh5a@gmail.com>
2026-01-04 22:05:59 +0700

    Update Router.cpp

--- src/Router.cpp
+++ src/Router.cpp
@@ -4,7 +4,7 @@
 Router::Router(const Config& config) : cfg(config), busyUntil(0.0) {}
 
 void Router::receivePacket(Packet p) {
-    // 1. Kiểm tra tràn hàng đợi (Queue Overflow)
+    // 1. Kiểm tra tràn hàng đợi 
     if (waitingQueue.size() >= cfg.max_queue_size) {
         // Gói tin bị DROP tại đây. -- > Không Push
         return; 
@@ -20,23 +20,23 @@ void Router::process(double currentTime) {
         return; 
     }
 
-    // Nếu Router đang rảnh và có hàng chờ
+    // Nếu Router đang rảnh và hàng chờ không đầy
     if (!waitingQueue.empty()) {
-        // 1. Lấy gói ưu tiên nhất ra
+        // 1. Lấy gói ưu tiên nhất ra xử lý
         Packet p = waitingQueue.top();
         waitingQueue.pop();
 
-        // 2. Gán thời điểm bắt đầu xử lý (chính là lúc Router vừa rảnh)
+        // 2. Gán thời điểm bắt đầu xử lý 
         p.startProcessTime = currentTime;
 
-        // 3. Tính toán thời gian truyền (Serialization Delay)
+        // 3. Tính thời gian truyền (Serialization Delay)
         // Công thức: Thời gian (s) = Size (bits) / Bandwidth (bps)
         double transmissionTime = (p.size * 8.0) / (cfg.bandwidth_mbps * 1e6);
 
-        // 4. Cập nhật thời điểm Router sẽ rảnh tiếp theo (Busy Until)
+        // 4. Cập nhật thời điểm Router sẽ rảnh tiếp theo
         busyUntil = currentTime + transmissionTime;
 
-        // 5. Gán thời gian hoàn thành (Finish Time) cho gói tin
+        // 5. Gán thời gian hoàn thành cho gói tin
         p.finishTime = busyUntil;
 
         // 6. Lưu vào lịch sử 
